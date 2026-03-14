958d0beefc772b0ea2f88228e16803a9036f4535
Shubham Paste <paste.shub404@gmail.com>
2026-03-14 23:04:30 +0530

    deployment issue fixed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,10 @@ const OTPRequest = require('../models/OTPRequest');
 const router = express.Router();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
